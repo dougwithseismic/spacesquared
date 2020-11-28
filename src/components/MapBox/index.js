8617aa38445cd3a642e24a31963ebc6108355e0c
Doug Silkstone <doug@withseismic.com>
2020-11-28 16:25:39 +0100

    GeoLocate

--- src/components/MapBox/index.js
+++ src/components/MapBox/index.js
@@ -1,20 +1,14 @@
 import { useState, useRef, useEffect } from 'react'
-import ReactMapGL, { Marker, GeolocateControl  } from 'react-map-gl'
+import ReactMapGL, { Marker, GeolocateControl, Layer } from 'react-map-gl'
 
 export default function Map({ locations }) {
   const targetRef = useRef()
   const [
-    selectLocation,
-    setSelectedLocation
-  ] = useState({})
+    userLocation,
+    setUserLocation
+  ] = useState({ latitude: 50.0755, longitude: 14.4378 })
 
-  const [
-    dimensions,
-    setDimensions
-  ] = useState({
-    width: 100,
-    height: 100
-  })
+  console.log('userLocation', userLocation)
 
   const [
     viewport,
@@ -27,29 +21,46 @@ export default function Map({ locations }) {
     pitch: 40
   })
 
+  const parkLayer = {
+    id: 'landuse_park',
+    type: 'fill',
+    source: 'mapbox',
+    'source-layer': 'landuse',
+    filter: [
+      '==',
+      'class',
+      'park'
+    ]
+  }
+
   return (
     <div className="w-screen h-screen" ref={targetRef}>
       <ReactMapGL
         width="100vw"
         height="100vh"
         mapStyle="mapbox://styles/dougwithseismic/cki1n30yt35b919mj4n6ksqv7"
-      
         mapboxApiAccessToken={process.env.MAPBOX_KEY}
         {...viewport}
         onViewportChange={(nextViewport) => {
           setViewport(nextViewport)
-          console.log(nextViewport)
         }}
         GeolocateControl
       >
-        <Marker latitude={50.0755} longitude={14.4378} offsetLeft={-20} offsetTop={-10}>
-          <div className="mark">You are here</div>
+        <Marker latitude={userLocation.latitude} longitude={userLocation.longitude} offsetLeft={-20} offsetTop={-10}>
+          <div className="mark" style={{ padding: '1em', backgroundColor: 'white' }}>
+            You are here
+          </div>
         </Marker>
-        <GeolocateControl 
-        positionOptions={{ enableHighAccuracy: true }} 
-        trackUserLocation={true}
-        style={{ position: 'absolute', padding: '1em', right: 0}}
-         />
+        <GeolocateControl
+          positionOptions={{ enableHighAccuracy: true }}
+          trackUserLocation={true}
+          style={{ position: 'absolute', padding: '1em', right: 0 }}
+          onViewportChange={(nextViewport) => {
+            setUserLocation({ latitude: nextViewport.latitude, longitude: nextViewport.longitude })
+            setViewport(nextViewport)
+          }}
+        />
+        <Layer {...parkLayer} paint={{ 'fill-color': '#dea' }} />
       </ReactMapGL>
     </div>
   )
