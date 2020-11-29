3cb502b4b5f57b54c8c7348502de96ff93b875f5
Doug Silkstone <doug@withseismic.com>
2020-11-29 15:11:05 +0100

    Testing Geolocate

--- src/components/MapBox/index.js
+++ src/components/MapBox/index.js
@@ -1,37 +1,54 @@
-import { useState, useRef, useContext } from 'react'
-import ReactMapboxGl, { Layer, Feature, MapContext } from 'react-mapbox-gl'
+import { useState, useRef, useContext, useEffect } from 'react'
+import ReactMapboxGl, { Layer, Feature, MapContext, ZoomControl } from 'react-mapbox-gl'
 import { GeolocateControl } from 'mapbox-gl'
 
 import SiteContext from '../../context/siteContext'
 
 export default function Map({ locations }) {
-  const { setSelectedEvent } = useContext(SiteContext)
-
-  const targetRef = useRef()
-  const [
-    userLocation,
-    setUserLocation
-  ] = useState({ latitude: 50.0755, longitude: 14.4378 })
-
   const [
     viewport,
     setViewport
   ] = useState({
-    // The latitude and longitude of the center of London
     latitude: 50.0755,
     longitude: 14.4378,
-    zoom: 14,
-    pitch: 40
+    zoom: [
+      15
+    ],
+    pitch: [
+      40
+    ]
   })
+
+  const [
+    userLocation,
+    setUserLocation
+  ] = useState({})
 
   const Map = ReactMapboxGl({
     accessToken: process.env.MAPBOX_KEY
   })
 
+  // GeoLocate
+  const geolocate = new GeolocateControl({
+    positionOptions: {
+      enableHighAccuracy: true
+    },
+    fitBoundsOptions: { maxZoom: 20 },
+    trackUserLocation: true
+  })
+
+  useEffect(() => {
+    // When a geolocate happens, let's share
+    geolocate.on('geolocate', (event) => {
+      console.log('A geolocate event has occurred.', event)
+      setUserLocation({ coords: event.coords })
+    })
+  }, [])
+
   return (
-    <div className="w-screen h-screen" ref={targetRef}>
+    <div className="w-screen h-screen">
       <Map
-        style="mapbox://styles/mapbox/streets-v9"
+        style="mapbox://styles/mapbox/streets-v11"
         containerStyle={{
           height: '100vh',
           width: '100vw'
@@ -40,12 +57,14 @@ export default function Map({ locations }) {
           viewport.longitude,
           viewport.latitude
         ]}
+        {...viewport}
       >
         <MapContext.Consumer>
           {(map) => {
-            map.addControl(new GeolocateControl())
+            map.addControl(geolocate)
           }}
         </MapContext.Consumer>
+        <ZoomControl position="bottom-right" />
         <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
           <Feature
             coordinates={[
